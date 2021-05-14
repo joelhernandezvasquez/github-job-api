375c7c4d7555a7b1feedbae328b5dbb80e34fdf9
joelhernandezvasquez <jhernandez98@outlook.es>
2021-05-14 14:19:52 -0400

    another push

--- src/components/DetailPage.js
+++ src/components/DetailPage.js
@@ -1,24 +1,55 @@
 import React,{useRef,useLayoutEffect} from 'react';
 import '../sass/utilities.scss';
 import '../sass/detail-page.scss';
+import useDimension from './hooks/useDimension';
 
 const DetailPage = (props) => {
    
-
+   const viewport = useDimension();
    const job = props.location.jobInfo
    const jobDescriptionRef = useRef();
+   const howApplyDescription = useRef();
    console.log(job);
 
    useLayoutEffect(() => {
     
     jobDescriptionRef.current.innerHTML = job.dataJob.description;
-     
-    //jobDescriptionRef.current.querySelectorAll("p")
+    howApplyDescription.current.innerHTML = job.dataJob.how_to_apply;
+
+    const links = document.querySelectorAll("a");
+
+    links.forEach((link=>{
+        link.setAttribute("target","blank");
+    }))
+    console.log(links);
+   
    }, [])
+
+    const renderFooter = () =>{
+
+        if(viewport.width < 768)
+        {
+            return (
+                <a className="btn default-btn" href={job.dataJob.url}>Apply Now</a>
+            )
+            
+        }
+
+        else{
+            return (
+               <div className="inner-container">
+                 <h2>{job.dataJob.company}</h2>
+                <a className="btn default-btn" href={job.dataJob.url}>Apply Now</a>
+               </div>
+                
+                
+            )
+        }
+    }
    
     
     return (
-        <div class="detail-page-wrapper">
+        <section class="detail-page-wrapper">
             <div className="container-detail">
             <div className="header container">
                 <div className="logo-container">
@@ -31,7 +62,7 @@ const DetailPage = (props) => {
                 </div>
            </div>
 
-           <div className="job-details-container container top-spacing">
+           <section className="job-details-container container top-spacing">
                 <div className="job-header-container">
                     <div className="job-header-info">
                         <div class="info">
@@ -50,14 +81,36 @@ const DetailPage = (props) => {
                 </div>
 
                 <div ref={jobDescriptionRef} className="job-description-container top-spacing">
-                    
+               
                 </div> 
+            </section>
+
+          <section className="apply-container container top-spacing ">
+            <h2> How to Apply</h2>
+            <div className="apply-details top-spacing">
+            <p>
+                Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu 
+                pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis, 
+                adipiscing varius, adipiscing in, lacinia vel, tellus.
+            </p>
+             <p ref={howApplyDescription}> </p>
             </div>
+          </section>
+
+          <section className="job-details-footer">
+            <div className="container">
+            {renderFooter()}
+           
             </div>
+             
+          </section>
+
+      </div>
+</section>
 
             
            
-        </div>
+       
     )
 }
 export default DetailPage;
